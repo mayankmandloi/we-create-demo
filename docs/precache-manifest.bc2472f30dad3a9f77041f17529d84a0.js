b88141c39a7f5ad68766d678b25f4a22ca95e825
unknown <mayankmandloi009@gmail.com>
2020-05-27 00:04:31 +0530

    dummy doc

---
+++ docs/precache-manifest.bc2472f30dad3a9f77041f17529d84a0.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efb058cd26c4bcff0639b14ce811d6b6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3b4e8cbb0e3e5bc4cce0",
+    "url": "/static/css/main.d4605112.chunk.css"
+  },
+  {
+    "revision": "634d90ed7780fb91a2ce",
+    "url": "/static/js/2.17d17ac5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.17d17ac5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b4e8cbb0e3e5bc4cce0",
+    "url": "/static/js/main.b9a0615b.chunk.js"
+  },
+  {
+    "revision": "2c261a71d90a9e2c42c1",
+    "url": "/static/js/runtime-main.2c656aff.js"
+  },
+  {
+    "revision": "72dbb38f18538e16440a91e9372d7ca4",
+    "url": "/static/media/Product Design.72dbb38f.png"
+  },
+  {
+    "revision": "ba965527291f385512bf62a1e7a9729c",
+    "url": "/static/media/discovery-white.ba965527.svg"
+  },
+  {
+    "revision": "5f0d824fa676d13c3ac1a671e9a82be8",
+    "url": "/static/media/more than visual design.5f0d824f.png"
+  },
+  {
+    "revision": "bb65159186ec535226eecfec20b1b6b7",
+    "url": "/static/media/research-and-project-estimation.bb651591.svg"
+  },
+  {
+    "revision": "9acdac4bbfa330805e7f2ea665ac7050",
+    "url": "/static/media/ui.9acdac4b.svg"
+  },
+  {
+    "revision": "b9cb93128b535d4e22e179838b4d24bf",
+    "url": "/static/media/user experience design.b9cb9312.svg"
+  },
+  {
+    "revision": "b6e6cbf47be4f9654bec1d789ab1d789",
+    "url": "/static/media/visual-design.b6e6cbf4.svg"
+  },
+  {
+    "revision": "6b180f991c0c6baf03c69519cd9488aa",
+    "url": "/static/media/wireframe.6b180f99.svg"
+  }
+]);
